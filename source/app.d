8951abe697470f3fcc8064d3072c6580d7fc38f8
Simon Kérouack <ekyo777@gmail.com>
2013-06-02 12:24:12 -0400

    added main, first compilation successful

---
+++ source/app.d
@@ -0,0 +1,9 @@
+/**
+   Copyright: © 2013 Simon Kérouack.
+   License: Subject to the terms of the MIT license, as written in the included LICENSE.txt file.
+   Authors: Simon Kérouack
+*/
+
+int main(string[] args) {
+  return 0;
+}
